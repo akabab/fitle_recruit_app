d6c18572f67ee3a9e3b5ed7be924b717b022cc87
akabab <ycribier@student.42.fr>
2015-11-18 10:07:10 +0100

    Initial Commit

---
+++ FitleRecruitAppTests/FitleRecruitAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  FitleRecruitAppTests.swift
+//  FitleRecruitAppTests
+//
+//  Created by Yoann Cribier on 18/11/15.
+//  Copyright © 2015 Yoann Cribier. All rights reserved.
+//
+
+import XCTest
+@testable import FitleRecruitApp
+
+class FitleRecruitAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
